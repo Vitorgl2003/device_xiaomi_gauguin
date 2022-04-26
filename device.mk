82906fd01fb9830ab86ca3840c1d01894009f403
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-25 20:56:35 -0400

    gauguin: Uprev Bluetooth audio HIDL impl to 2.1
    Change-Id: I63211c9877c19d089e7cafa5a3cc52a888309dca
    Signed-off-by: Vitorgl2003 <vitorgl.2003@gmail.com>

--- device.mk
+++ device.mk
@@ -172,7 +172,7 @@ BOARD_HAVE_QCOM_FM := false
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
